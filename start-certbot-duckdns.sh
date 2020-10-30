8c52b3a83410d87ecae990ab4335645696b78a73
redxef <federanko.manuel@gmail.com>
2020-10-30 11:58:53 +0100

    Fixed timout and other stuff.

--- start-certbot-duckdns.sh
+++ start-certbot-duckdns.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env sh
 
-set -x
-
 dhparamfile='/etc/letsencrypt/ssl-dhparams.pem'
 ssl_includefile='/etc/letsencrypt/options-ssl-nginx.conf'
 
 echo "Updating duckdns record"
 duckdns
 echo "Acquiering letsencrypt certificates"
-certbot-duckdns
+certbot-duckdns "$@"
 echo "Starting crond"
 exec crond -f
